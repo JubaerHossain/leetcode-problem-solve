0ffa52b1c5a7b3d5c39db93421863c206540a333
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-28 23:53:18 +0600

    array

--- main.cpp
+++ main.cpp
@@ -1,33 +1,42 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-using namespace std;
-
+#include <bits/stdc++.h>
+using namespace std; 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        for(auto i = nums.begin(); i != nums.end(); i++){
-            auto j = find(i+1, nums.end(), target - *i);
-            if(j != nums.end()){
-                result.push_back(i - nums.begin());
-                result.push_back(j - nums.begin());
-                break;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        if (n > m) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int l = 0, r = n;
+        int k = (n + m + 1) / 2;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums1[mid] < nums2[k - mid - 1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
             }
         }
-        return result;
+        int a = max(l > 0 ? nums1[l - 1] : INT_MIN, k - l > 0 ? nums2[k - l - 1] : INT_MIN);
+        if ((n + m) % 2 == 1) {
+            return a;
+        }
+        int b = min(l < n ? nums1[l] : INT_MAX, k - l < m ? nums2[k - l] : INT_MAX);
+        return (a + b) / 2.0;
+
+
         
     }
 };
 
-
-int main(int argc, char const *argv[])
+int main()
 {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
     Solution s;
-    vector<int> result = s.twoSum(nums, target);
-     for(auto k = result.begin(); k != result.end(); k++)
-         cout << *k << endl;
+    vector<int> nums1 = {1, 3};
+    vector<int> nums2 = {2};
+
+    cout << s.findMedianSortedArrays(nums1, nums2) << endl;
+    
     return 0;
 }
