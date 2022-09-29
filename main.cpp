1a3fe1e40eebc403a184780f0839bc3efac78a85
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-29 07:55:15 +0600

    d

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+        }
+        
+        
+    }
+};
+
 int main()
 {
+
     
     return 0;
 }
