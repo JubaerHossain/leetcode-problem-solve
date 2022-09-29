6a57ab9ac3bbb93f21a1fe78d6682c86d9269cc6
jubaer.sookh <jubaer@sookh.com>
2022-09-29 16:36:04 +0600

    start

--- main.cpp
+++ main.cpp
@@ -1,34 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
-public:
-    int searchInsert(vector<int> &nums, int target)
-    {
-        int n = nums.size();
-        int l = 0, r = n - 1;
-        while (l <= r)
-        {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] < target)
-                l = mid + 1;
-            else
-                r = mid - 1;
-        }
-        return l;
-    }
-};
-
 int main()
 {
-    Solution s;
 
-    vector<int> nums = {1, 3, 5, 6};
-
-    cout << s.searchInsert(nums, 5) << endl;
+    cout << "Hello World" << endl;
 
     return 0;
 }
