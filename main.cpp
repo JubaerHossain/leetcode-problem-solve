5830aa39fe68802ab814a6cf6e76e4ca5dfd94e4
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-29 08:10:26 +0600

    done

--- main.cpp
+++ main.cpp
@@ -7,19 +7,57 @@ public:
         int low = 0, high = nums.size() - 1;
         while (low <= high)
         {
+            cout << low << " " << high << endl;
             int mid = (low + high) / 2;
+            cout << "mid" << mid << endl;
             if (nums[mid] == target)
             {
-                return mid;
+                int left = mid, right = mid;
+                cout << "left" << left << " and " << "right" << right << endl;
+                while (left > 0 && nums[left - 1] == target)
+                    left--;
+                while (right < nums.size() - 1 && nums[right + 1] == target)
+                    right++;
+                return {left, right};
+            }
+            if (nums[mid] < nums[high])
+            {
+                if (nums[mid] < target && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            else
+            {
+                if (nums[low] <= target && target < nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
             }
         }
-        
+        return {-1, -1};        
         
     }
 };
 
 int main()
 {
+    Solution s;
+    vector<int> nums = {5,7,7,8,8,10};
+    int target = 8;
+    vector<int> res = s.searchRange(nums, target);
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
 
     
     return 0;
