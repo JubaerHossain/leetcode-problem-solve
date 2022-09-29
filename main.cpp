18ac80c6b149a58dd432eafdcc71a6fb0b060912
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-29 07:48:31 +0600

    search using stl

--- main.cpp
+++ main.cpp
@@ -29,6 +29,33 @@ public:
         }
         return -1;
     }
+    int search2(vector <int> & nums, int target){
+        int low = 0, high = nums.size() - 1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target ) return mid;
+            if(nums[mid] < nums[high]){
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }else{
+                if(nums[low] <= target && target < nums[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+        }
+    }
+    int search3(vector <int> & nums, int target){
+        int low = 0, high = nums.size() - 1;
+        sort(nums.begin(), nums.end());
+
+      return  binary_search(nums.begin(), nums.end(), target);
+    }
 };
 
 int main()
@@ -38,6 +65,16 @@ int main()
     vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
     int target = 0;
     cout << s.search(nums, target) << endl;
+
+
+    cout <<"using stl : " << endl;
+    int res = s.search3(nums, 10);
+
+    if(res == 1){
+        cout << "found" << endl;
+    }else{
+        cout << "not found" << endl;
+    }
 
     return 0;
 }
