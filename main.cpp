97c5164f3ca865b52fadc4b8bd7d161bde358c87
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-30 21:41:00 +0600

    not done

--- main.cpp
+++ main.cpp
@@ -1,42 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-class Solution {
+class Solution
+{
 public:
-    int findMin(vector<int>& nums) {
+    int smallestDivisor(vector<int> &nums, int threshold)
+    {
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+        int ans = high;
 
-        int n = nums.size();
-        int l = 0, r = n - 1;
-        while (l < r) {
-            int mid = (l + r ) / 2;
-            if (nums[mid] == nums[r]) {
-                r = mid - 1;
-            }else if (nums[mid] < nums[r]) {
-                r = mid;
-            } else {
-                l = mid + 1;
+        while (low < high)
+        {
+            int mid = (high + low) / 2;
+            int sum = 0;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                sum += (nums[i] / mid);
+                
+                if (nums[i] % mid == 0)
+                {
+                    sum += 1;
+                }
+            }
+            if (sum <= threshold)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
             }
         }
-        return nums[l];
-
-        
-    }
-
-    int findMinSTL(vector<int>& nums) {
-        return *min_element(nums.begin(), nums.end());
+        return ans;
     }
 };
 
 int main()
 {
-
-    vector<int> nums = {1,3,3};
     Solution s;
-    cout << s.findMin(nums) << endl;
-
-    cout << s.findMinSTL(nums) << endl;
-
-
+    vector<int> nums = {1, 2, 5, 9};
+    int threshold = 6;
+    cout << s.smallestDivisor(nums, threshold);
 
     return 0;
 }
