9eb94bd2716b0b93cf63e6bb1c5da275ca19c6da
JubaerHossain <jubaer01.cse@gmail.com>
2022-10-15 18:50:58 +0600

    done

--- main.cpp
+++ main.cpp
@@ -17,31 +17,52 @@
 using namespace std; 
 class Solution {
 public:
- vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans = {};
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i< n-2; i++ ){
 
-     int l=0,r=numbers.size()-1;
-     while(l<r){
-         if(numbers[l]+numbers[r]==target){
-             return {l+1,r+1};
-         }
-         else if(numbers[l]+numbers[r]>target){
-             r--;
-         }
-         else{
-             l++;
-         }
-     }
-     return {};                   
+            if(nums[i] > 0) break;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+           
+            int l = i+1;
+            int r = n-1;
+            while(l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum < 0){
+                    l++;
+                }
+                else if(sum > 0){
+                    r--;
+                }
+                else{
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    while(l<r && nums[l] == nums[l+1]) l++;
+                    while(l<r && nums[r] == nums[r-1]) r--;
+                    l++;
+                    r--;
+                }
+            }
+            
+        }
+        return ans;
+        
     }
 };
 int main()
 {
     Solution s;
-    vector<int> v = {2,7,11,15};
-    int target = 9;
-    vector<int> ans = s.twoSum(v,target);
+    vector<int> v = {-1,0,1,2,-1,-4};
+    vector<vector<int>> ans = s.threeSum(v);
     for(auto i:ans)
-        cout<<i<<" ";
+    {
+        for(auto j:i)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
 
 
     return 0;
