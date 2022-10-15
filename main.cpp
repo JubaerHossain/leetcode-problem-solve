79786638ab86240fc418486b63f9a3a1078f0bfc
JubaerHossain <jubaer01.cse@gmail.com>
2022-10-15 19:08:29 +0600

    incomplete

--- main.cpp
+++ main.cpp
@@ -18,22 +18,11 @@ using namespace std;
 
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        int left[n], right[n];
-        left[0] = height[0];
-        for(int i = 1; i < n; i++) {
-            left[i] = max(left[i-1], height[i]);
-        }
-        right[n-1] = height[n-1];
-        for(int i = n-2; i >= 0; i--) {
-            right[i] = max(right[i+1], height[i]);
-        }
-        int ans = 0;
-        for(int i = 0; i < n; i++) {
-            ans += min(left[i], right[i]) - height[i];
-        }
-        return ans;
+    int getLengthOfOptimalCompression(string s, int k) {
+
+        int n = s.size();
+
+        
         
     }
 };
@@ -41,8 +30,10 @@ public:
 int main()
 {
     Solution s;
-    vector<int> v= {0,1,0,2,1,0,1,3,2,1,2,1};
-    cout<<s.trap(v);
+    string str = "aabbaa";
+    int k = 2;
+    cout<<s.getLengthOfOptimalCompression(str, k)<<endl;
+
 
 
     return 0;
