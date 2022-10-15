209b9f9e8287cc3d01936680291fb5fa22af36b7
JubaerHossain <jubaer01.cse@gmail.com>
2022-10-15 23:26:22 +0600

    g

--- main.cpp
+++ main.cpp
@@ -18,11 +18,21 @@ using namespace std;
 
 class Solution {
 public:
-    int getLengthOfOptimalCompression(string s, int k) {
-
-        int n = s.size();
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
 
         
+
         
     }
 };
@@ -30,9 +40,9 @@ public:
 int main()
 {
     Solution s;
-    string str = "aabbaa";
-    int k = 2;
-    cout<<s.getLengthOfOptimalCompression(str, k)<<endl;
+    vector<int> nums1 = {1,2,3,0,0,0};
+    vector<int> nums2 = {2,5,6};
+    s.merge(nums1, 3, nums2, 3);
 
 
 
