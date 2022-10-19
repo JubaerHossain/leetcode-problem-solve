cf450d731b3a890a305737facde225cf409aa53d
jubaer.sookh <jubaer@sookh.com>
2022-10-19 18:17:25 +0600

    s

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while (l < r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r])
+            {
+                l = mid + 1;
+            }
+            else if (nums[mid] < nums[r])
+            {
+                r = mid;
+            }
+            else
+            {
+                r = mid--;
+            }
+        }
+        return nums[l];
+    }
+};
+
 int main()
 {
-
-    cout << "Hello World" << endl;
+    Solution s;
+    vector<int> nums = {3, 4, 5, 1, 2};
+    cout << s.findMin(nums) << endl;
 
     return 0;
 }
