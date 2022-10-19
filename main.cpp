0d3860a096a060413a4511cf8a0f11f8dd75a71d
jubaer.sookh <jubaer@sookh.com>
2022-10-19 18:28:29 +0600

    s

--- main.cpp
+++ main.cpp
@@ -1,39 +1,32 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std; 
 
-class Solution
-{
+class Solution {
 public:
-    int findMin(vector<int> &nums)
-    {
-
-        int n = nums.size();
-        int l = 0, r = n - 1;
-        while (l < r)
-        {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] > nums[r])
-            {
-                l = mid + 1;
-            }
-            else if (nums[mid] < nums[r])
-            {
-                r = mid;
-            }
-            else
-            {
-                r = mid--;
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+                char t = st.top(); st.pop();
+                if (c == ')' && t != '(') return false;
+                if (c == ']' && t != '[') return false;
+                if (c == '}' && t != '{') return false;
             }
         }
-        return nums[l];
+        return st.empty();
+        
     }
 };
 
 int main()
 {
     Solution s;
-    vector<int> nums = {3, 4, 5, 1, 2};
-    cout << s.findMin(nums) << endl;
+    string str = "()";
+    cout << s.isValid(str) << endl;
 
+    
     return 0;
 }
