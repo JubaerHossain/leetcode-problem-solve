c90befaabfafb084271580a222fb7c537bbe04b8
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-05 10:29:55 +0600

    1st done

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
 #define ld long double
 #define pb push_back
@@ -7,32 +7,36 @@
 #define ppf pop_front
 #define maxe *max_element
 #define mine *min_element
-#define mem(a,b) memset(a,b,sizeof(a))
-#define all(a) (a).begin(),(a).end()
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) ((a)*((b)/gcd(a,b)))
-#define point(a) fixed<<setprecision(a)
-#define uniq(a) sort(all(a)); (a).erase(unique((a).begin(),(a).end()),(a).end())
-#define tc int test; cin>>test; while(test--)
-using namespace std; 
-
-
-
+#define mem(a, b) memset(a, b, sizeof(a))
+#define all(a) (a).begin(), (a).end()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) * ((b) / gcd(a, b)))
+#define point(a) fixed << setprecision(a)
+#define uniq(a)   \
+    sort(all(a)); \
+    (a).erase(unique((a).begin(), (a).end()), (a).end())
+#define tc       \
+    int test;    \
+    cin >> test; \
+    while (test--)
+using namespace std;
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+    int countDigitOne(int n) {
+        int count = 0;
+        for (long long i = 1; i <= n; i *= 10) {
+            long long divider = i * 10;
+            count += (n / divider) * i + min(max(n % divider - i + 1, 0LL), i);
+        }
+        return count;
     }
 };
 
 int main()
 {
+
     Solution s;
-    vector<int> nums1 = {1,2,3,0,0,0};
-    vector<int> nums2 = {2,5,6};
-    s.merge(nums1, 3, nums2, 3);
-
-
+    cout << s.countDigitOne(13) << endl;
 
     return 0;
-};
+}
