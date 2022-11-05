e606bd22adab78dc87a7687fac90a0e3bfc29abd
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-05 09:45:22 +0600

    s

--- main.cpp
+++ main.cpp
@@ -16,13 +16,7 @@
 #define tc int test; cin>>test; while(test--)
 using namespace std; 
 
-struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+
 
 class Solution {
 public:
