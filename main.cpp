d9357cc81590820e9f42ee24e90a947e6527b305
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-05 09:44:43 +0600

    s

--- main.cpp
+++ main.cpp
@@ -16,23 +16,17 @@
 #define tc int test; cin>>test; while(test--)
 using namespace std; 
 
+struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1, j = n - 1, k = m + n - 1;
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
-            }
-        }
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
-        }
-
-        
-
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
     }
 };
