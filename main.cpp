c4126f817a862c30c78ecbdabd70d72fc6cff8f1
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-16 07:45:44 +0600

    accepted

--- main.cpp
+++ main.cpp
@@ -22,6 +22,9 @@ public:
         int n = nums.size();
         int count = 0;
         for(int i = 0; i < n; i++){
+            cout << nums[i] << " ";
+            cout << endl;
+            cout <<" nums[(i+1)%n] " << nums[(i+1)%n] << endl;
             if(nums[i] > nums[(i+1)%n]) count++;
         }
         return count <= 1;
