8b185d8d93e4eb6cb5d15ec7bd746ac40e518a47
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-16 07:00:31 +0600

    accepted

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define ll long long
 #define ld long double
 #define pb push_back
@@ -7,71 +7,32 @@
 #define ppf pop_front
 #define maxe *max_element
 #define mine *min_element
-#define mem(a, b) memset(a, b, sizeof(a))
-#define all(a) (a).begin(), (a).end()
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a) * ((b) / gcd(a, b)))
-#define point(a) fixed << setprecision(a)
-#define uniq(a)   \
-    sort(all(a)); \
-    (a).erase(unique((a).begin(), (a).end()), (a).end())
-#define tc       \
-    int test;    \
-    cin >> test; \
-    while (test--)
-using namespace std;
-class Solution
-{
-public:
-    vector<int> sortArray(vector<int> &nums)
-    {
-        mergeSort(nums, 0, nums.size() - 1);
-        return nums;
-    }
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(a) (a).begin(),(a).end()
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a)*((b)/gcd(a,b)))
+#define point(a) fixed<<setprecision(a)
+#define uniq(a) sort(all(a)); (a).erase(unique((a).begin(),(a).end()),(a).end())
+#define tc int test; cin>>test; while(test--)
+using namespace std; 
 
-    void mergeSort(vector<int> &nums, int low, int high)
-    {
-        if (low == high)
-            return;
-        int mid = (high - low) / 2 + low;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid + 1, high);
-        merge2(nums, low, mid, high);
-    }
-    void merge2(vector<int> &nums, int low, int mid, int high)
-    {
-        if (low >= high)
-            return;
-        int l = low, r = mid + 1, k = 0, size = high - low + 1;
-        vector<int> sorted(size, 0);
-        while (l <= mid and r <= high)
-            sorted[k++] = nums[l] < nums[r] ? nums[l++] : nums[r++];
-        while (l <= mid)
-            sorted[k++] = nums[l++];
-        while (r <= high)
-            sorted[k++] = nums[r++];
-        for (k = 0; k < size; k++)
-            nums[k + low] = sorted[k];
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > nums[(i+1)%n]) count++;
+        }
+        return count <= 1;
     }
 };
 
 int main()
 {
     Solution s;
-    // vector<int> a = {-2, 3, -5};
-    vector<int> a = {5,2,3,1};
-    for (int i = 0; i < a.size(); i++)
-    {
-        cout << a[i] << " ";
-    }
-    vector<int> b = s.sortArray(a);
-
-    cout << " after sorting " << endl;
-
-    for (int i = 0; i < b.size(); i++)
-    {
-        cout << b[i] << " ";
-    }
-
+    vector<int> v = {3,4,5,1,2};
+    cout<<s.check(v)<<endl;
+    
     return 0;
 }
