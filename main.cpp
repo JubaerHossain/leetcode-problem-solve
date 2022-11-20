f14f5d3a7ef50f436f73eb8b301c16179d4d7add
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-20 07:03:19 +0600

    accepted

--- main.cpp
+++ main.cpp
@@ -24,28 +24,6 @@ using namespace std;
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        // int low = 0, high = nums.size() -1;
-
-        // while(low <= high){
-        //     int mid = low + (high -low )/ 2;
-        //     if(nums[mid] == target) return mid;
-        //     if(nums[low] <= nums[mid]){
-        //         if(nums[low] <= target && nums[mid] >= target){
-        //             high = mid-1;
-        //         }else{
-        //             low = mid +1;
-        //         }
-        //     }else{
-        //         if(nums[high] >= target && nums[mid] <= target){                   
-        //              low = mid +1;
-        //         }else{
-        //              high = mid-1;
-        //         }
-
-        //     }
-        // }
-        // return -1;
-
 
         int low =0, high = nums.size() - 1;
 
