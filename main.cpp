3f4e922ee64e734fc20e0102160f6fdec6684389
JubaerHossain <jubaer01.cse@gmail.com>
2022-12-03 10:17:02 +0600

    accepted

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define ll long long
 #define ld long double
 #define pb push_back
@@ -7,52 +7,48 @@
 #define ppf pop_front
 #define maxe *max_element
 #define mine *min_element
-#define mem(a, b) memset(a, b, sizeof(a))
-#define all(a) (a).begin(), (a).end()
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a) * ((b) / gcd(a, b)))
-#define point(a) fixed << setprecision(a)
-#define uniq(a)   \
-    sort(all(a)); \
-    (a).erase(unique((a).begin(), (a).end()), (a).end())
-#define tc       \
-    int test;    \
-    cin >> test; \
-    while (test--)
-using namespace std;
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(a) (a).begin(),(a).end()
+#define gcd(a,b) __gcd(a,b)
+#define sz(a) (int)(a).size()
+#define lcm(a,b) ((a)*((b)/gcd(a,b)))
+#define point(a) fixed<<setprecision(a)
+#define uniq(a) sort(all(a)); (a).erase(unique((a).begin(),(a).end()),(a).end())
+#define tc int test; cin>>test; while(test--)
+using namespace std; 
+
+
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() -1;
-
-        while(low <= high){
-            int mid = low + (high -low )/ 2;
-            if(nums[mid] == target) return mid;
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && nums[mid] >= target){
-                    high = mid-1;
-                }else{
-                    low = mid +1;
-                }
-            }else{
-                if(nums[high] >= target && nums[mid] <= target){                   
-                     low = mid +1;
-                }else{
-                     high = mid-1;
-                }
-
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int l = 1, r = 1e9;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            int time =  0;
+            for(int i = 0; i < n; i++) {
+                time += (piles[i] + mid - 1) / mid;
+            }
+            if(time > h) {
+                l = mid + 1;
+            } else {
+                r = mid;
             }
         }
-        return -1;
+        return l;
+        
     }
 };
+
 int main()
 {
     Solution s;
-    vector<int> v = {4,5,6,7,0,1,2};
-    cout << s.search(v, 0) << endl;
-
-
+    // vector<int> piles = {3,6,7,11};
+    vector<int> piles = {30,11,23,4,20};
+    // int h = 8;
+    int h = 5;
+    cout<<s.minEatingSpeed(piles,h)<<endl;
+        
     return 0;
 }
